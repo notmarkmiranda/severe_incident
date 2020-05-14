c45528ff8ce0f3c80aced6843b2e448bd0318e6d
Mark Miranda <notmarkmiranda@gmail.com>
2020-05-13 20:07:48 -0600

    Add devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #validates_uniqueness_of :email
+  validates :email, uniqueness: { case_sensitive: true }
+end
---
+++ spec/requests/home_controller_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe HomeController, type: :request do
+  describe 'GET#index' do
+    subject(:get_index) { get root_path }
+
+    it 'should have 200 status' do
+      get_index
+
+      expect(response).to have_http_status(200)
+    end
+  end
+end
