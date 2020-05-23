d9d9c5bf866bf09c7be9727f8fe3cf03f5cd1a37
Mark Miranda <notmarkmiranda@gmail.com>
2020-05-23 07:47:44 -0600

    Add dashboard and redirect after login

---
+++ spec/features/dashboard/user_is_redirected_to_dashboard_after_login_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'User is redirected to dashboard after login', type: :feature do
+  let(:user) { create(:user) }
+  
+  it "redirects to dashboard and has a user's email" do
+    visit new_user_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Log in"
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content(user.email)
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -102,6 +102,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # include factory_bot methods in specs
+  config.include FactoryBot::Syntax::Methods  
 end
 
 Shoulda::Matchers.configure do |config|
