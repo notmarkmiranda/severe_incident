0438d759df5ec564168d9c5e4056f1492587e406
Mark Miranda <notmarkmiranda@gmail.com>
2020-05-24 13:42:42 -0600

    Add standardrb and standardize entire codebase

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
 
-  #validates_uniqueness_of :email
-  validates :email, uniqueness: { case_sensitive: true }
+  # validates_uniqueness_of :email
+  validates :email, uniqueness: {case_sensitive: true}
 end
--- spec/requests/home_controller_request_spec.rb
+++ spec/requests/home_controller_request_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe HomeController, type: :request do
-  describe 'GET#index' do
+  describe "GET#index" do
     subject(:get_index) { get root_path }
 
-    it 'should have 200 status' do
+    it "should have 200 status" do
       get_index
 
       expect(response).to have_http_status(200)
